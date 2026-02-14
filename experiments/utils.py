9bf8444ad8745b25c0a2c518e8cea5378d63b61a
Ali Hashish <Alimohamedhashish@gmail.com>
2026-02-14 15:36:10 +0200

    fat: add model utils to train and evaluate models

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,6 +1,4 @@
 import pandas as pd
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.metrics import confusion_matrix
 
 def load_data(path):
     """
@@ -30,52 +28,3 @@ def load_data(path):
     y_test = test_df["label"]
 
     return X_train, y_train, X_dev, y_dev, X_test, y_test
-
-def train_model(model, X_train, y_train):
-    """
-    Train the given model using the provided training data.
-    
-    :param model: sklearn model instance, the machine learning model to be trained.
-    :param X_train: df.DataFrame, the training features.
-    :param y_train: df.Series, The training labels
-
-    return: the trained model. 
-    """
-
-    model.fit(X_train, y_train)
-
-    return model
-    
-def evaluate_model(model, X_dev, y_dev):
-    """
-    Evaluate the performance of the trained model on the development set and return the evaluation metrics.
-    
-    :param model: trained model instance, the machine learning model to be evaluated.
-    :param X_dev: df.DataFrame, the development features.
-    :param y_dev: df.Series, The development labels.
-
-    Returns:
-    metrics: dict, dictionary containing the performance metrics as accuracy, precision, recall, and F1-score. 
-
-    """
-
-    y_pred = model.predict(X_dev)
-
-    accuracy = accuracy_score(y_dev, y_pred)
-    precision = precision_score(y_dev, y_pred)
-    recall = recall_score(y_dev, y_pred)
-    f1 = f1_score(y_dev, y_pred)
-    cm = confusion_matrix(y_dev, y_pred)
-
-    # strore them into metrics dict
-    metrics = {
-        "accuracy": accuracy,
-        "precision": precision,
-        "recall": recall,
-        "f1_score": f1,
-        "confusion_matrix": cm
-    }
-
-    return metrics
-
-
